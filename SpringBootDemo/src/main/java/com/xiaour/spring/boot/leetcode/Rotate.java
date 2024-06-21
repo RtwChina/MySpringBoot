eaadd40ed7d9d9f41ad90e36f0d09aa6061d17d9
阮天炜 <ruantianwei>
2024-06-21 21:58:25 +0800

    初始化

---
+++ SpringBootDemo/src/main/java/com/xiaour/spring/boot/leetcode/Rotate.java
@@ -0,0 +1,48 @@
+package com.xiaour.spring.boot.leetcode;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * https://leetcode.cn/leetbook/read/top-interview-questions-easy/x2skh7/
+ * 旋转数组
+ * @author rtw
+ * @since 2024/6/16
+ */
+public class Rotate {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        Rotate.rotateV2(nums,2);
+        System.out.println(JSON.toJSONString(nums));
+    }
+
+
+    private static void rotate(int[] nums, int k) {
+        int[] nums2 = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int after = (i + k) % nums.length;
+            nums2[after] = nums[i];
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            nums[i] = nums2[i];
+        }
+    }
+
+    private static void rotateV2(int[] nums, int k) {
+        // 翻转  7 6 5 4 3 2 1
+        reverse(nums, 0, nums.length - 1);
+        // 翻转  5 6 7
+        reverse(nums, 0, k % nums.length - 1);
+        // 翻转  1 2 3 4
+        reverse(nums, k % nums.length, nums.length - 1);
+
+    }
+
+    // 翻转数组  1234 -> 4321
+    private static void reverse(int[] nums, int start, int end) {
+        for (int i = start; i < ((end - start + 1) / 2) + start; i++) {
+            int temp = nums[i];
+            nums[i] = nums[end + start - i];
+            nums[end + start - i] = temp;
+        }
+    }
+}
---
+++ SpringBootDemo/src/main/java/com/xiaour/spring/boot/leetcode/Solution.java
@@ -0,0 +1,44 @@
+package com.xiaour.spring.boot.leetcode;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * https://leetcode.cn/leetbook/read/top-interview-questions-easy/x248f5/
+ * 存在重复元素
+ * @author rtw
+ * @since 2024/6/21
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,9};
+        boolean duplicate = Solution.containsDuplicate(array);
+        System.out.println(duplicate);
+    }
+
+
+
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i+1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean containsDuplicateV2(int[] nums) {
+        Set set =  new HashSet();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+
+        }
+        return false;
+    }
+}
